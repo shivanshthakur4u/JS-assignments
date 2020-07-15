f8507e827a2f43f3f0d0e9ee2c7a4dc1f4991eb1
Saurabh Singh <68112187+shivanshthakur4u@users.noreply.github.com>
2020-07-15 19:58:55 +0530

    Add files via upload

---
+++ Day2/Assignment 3.js
@@ -0,0 +1,8 @@
+var num1=window.prompt("Enter the age");
+if (num1==21)
+{
+    console.log("You Can drink")
+}
+else{
+    console.log("You Can't drink")
+}
---
+++ Day2/Assignment-1.js
@@ -0,0 +1,8 @@
+
+var num1 = window.prompt("Num1: ");
+var num2 = window.prompt("Num2: ");
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+console.log(num1 + num2);
